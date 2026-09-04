1d93105091ebeb459b9a106702e1d093ad7f1990
shidenglu <2442503334@qq.com>
2026-09-04 21:43:34 +0800

    feat：nin网络结构代码介绍

--- 10_study_deep_learning_by_self/chapter09_卷积神经网络/nin/code/nin.py
+++ 10_study_deep_learning_by_self/chapter09_卷积神经网络/nin/code/nin.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from d2l import torch as d2l
 
+# 卷积块
 def nin_block(in_channels, out_channels, kernel_size, strides, padding):
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
@@ -9,6 +10,7 @@ def nin_block(in_channels, out_channels, kernel_size, strides, padding):
         nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU(),
         nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU())
 
+# NiN网络
 net = nn.Sequential(
     nin_block(1, 96, kernel_size=11, strides=4, padding=0),
     nn.MaxPool2d(3, stride=2),
@@ -23,11 +25,13 @@ net = nn.Sequential(
     # 将四维的输出转成二维的输出，其形状为(批量大小,10)
     nn.Flatten())
 
+# 训练模型
 X = torch.rand(size=(1, 1, 224, 224))
 for layer in net:
     X = layer(X)
     print(layer.__class__.__name__,'output shape:\t', X.shape)
 
+# 由于NiN网络的卷积层和全连接层都比较大，训练起来非常慢。我们可以通过减少每个卷积层的通道数来构造一个更小的网络，从而加快训练速度。
 lr, num_epochs, batch_size = 0.1, 10, 128
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
 d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
